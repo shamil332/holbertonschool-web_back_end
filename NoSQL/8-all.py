cac7c88ca5f3ff05945c7fb1734d7c26cab42480
shamil332 <shamilkazimli2006@gmail.com>
2025-11-30 15:54:40 +0100

    task 8

---
+++ NoSQL/8-all.py
@@ -0,0 +1,5 @@
+# a Python function that lists all documents in a collection
+def list_all(mongo_collection):
+    if mongo_collection is None:
+        return []
+    return list(mongo_collection.find())
